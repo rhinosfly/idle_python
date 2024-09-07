3ffaab19c78cfa3ea47d38e82bd8bcc1764029fa
Linus <flyrhinosfly@gmail.com>
2024-09-06 20:36:53 -0400

    add working upgrade button; disorganized

--- idle_python.py
+++ idle_python.py
@@ -7,12 +7,23 @@ def increment():
     global money
     money += clickValue
 
+def upgradeClickValue():
+    global clickValue, clickValueCost, money, buttons
+    if money >= clickValueCost:
+        money -= clickValueCost
+        clickValueCost *= 2
+        clickValue += 1
+        buttons[1].name = str(clickValueCost) + ": click damage +1"
+
 pr.init_window(800,400,"idle game")
 pr.set_target_fps(60)
 
 money = 0
 clickValue = 1
-buttons = [Button.Button(600,100,50,50,pr.RED,"click me",increment)]
+clickValueCost = 20
+
+buttons = [Button.Button(600,100,50,50,pr.RED,"click me",increment),
+           Button.Button(20,100,50,20,pr.BLUE, str(clickValueCost) + ": click damage +1", upgradeClickValue)]
 
 
 
@@ -26,6 +37,7 @@ while not pr.window_should_close():
     for button in buttons:
         button.draw()
     pr.draw_text(f"{money}", 10, 10, 20, pr.WHITE)
+    pr.draw_text("click damage: " + str(clickValue), 10, 30, 20, pr.WHITE)
     pr.end_drawing()
 
 pr.close_window()
